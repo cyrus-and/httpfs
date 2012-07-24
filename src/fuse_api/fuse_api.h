59af659556bbc621692bf96b8853a40daa3c2452
Andrea Cardaci <cyrus.and@gmail.com>
2012-07-24 13:15:05 +0200

    Fix typos in FUSE API function list

--- src/fuse_api/fuse_api.h
+++ src/fuse_api/fuse_api.h
@@ -20,23 +20,23 @@ int phpfs_write( const char * , const char * , size_t , off_t , struct fuse_file
 int phpfs_statfs( const char * , struct statvfs * );
 int phpfs_flush( const char * , struct fuse_file_info * );
 int phpfs_release( const char * , struct fuse_file_info * );
-int phpfs_fsync( const char * , int phpfs_, struct fuse_file_info * );
-int phpfs_setxattr( const char * , const char * , const char * , size_t , int phpfs_ );
+int phpfs_fsync( const char * , int , struct fuse_file_info * );
+int phpfs_setxattr( const char * , const char * , const char * , size_t , int );
 int phpfs_getxattr( const char * , const char * , char * , size_t );
 int phpfs_listxattr( const char * , char * , size_t );
 int phpfs_removexattr( const char * , const char * );
 int phpfs_opendir( const char * , struct fuse_file_info * );
 int phpfs_readdir( const char * , void * , fuse_fill_dir_t , off_t , struct fuse_file_info * );
 int phpfs_releasedir( const char * , struct fuse_file_info * );
-int phpfs_fsyncdir( const char * , int phpfs_ , struct fuse_file_info * );
-int phpfs_access( const char * , int phpfs_ );
+int phpfs_fsyncdir( const char * , int , struct fuse_file_info * );
+int phpfs_access( const char * , int );
 int phpfs_create( const char * , mode_t , struct fuse_file_info * );
 int phpfs_ftruncate( const char * , off_t , struct fuse_file_info * );
 int phpfs_fgetattr( const char * , struct stat * , struct fuse_file_info * );
-int phpfs_lock( const char * , struct fuse_file_info * , int phpfs_cmd , struct flock * );
-int phpfs_utimens( const char * , const struct timespec tv[2] );
-int phpfs_bmap( const char * , size_t blocksize , uint64_t *idx );
-int phpfs_ioctl( const char * , int phpfs_cmd , void *arg , struct fuse_file_info * , unsigned int phpfs_flags , void *data );
-int phpfs_poll( const char * , struct fuse_file_info * , struct fuse_pollhandle *ph , unsigned *reventsp );
+int phpfs_lock( const char * , struct fuse_file_info * , int , struct flock * );
+int phpfs_utimens( const char * , const struct timespec [2] );
+int phpfs_bmap( const char * , size_t , uint64_t * );
+int phpfs_ioctl( const char * , int , void * , struct fuse_file_info * , unsigned int , void * );
+int phpfs_poll( const char * , struct fuse_file_info * , struct fuse_pollhandle * , unsigned * );
 
 #endif
