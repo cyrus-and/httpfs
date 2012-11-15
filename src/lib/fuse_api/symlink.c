737f171a66996bf60d19123a6875242c435915fc
Andrea Cardaci <cyrus.and@gmail.com>
2012-11-15 03:13:04 +0100

    Fix remote chroot path issue
    For callbacks that require another path as a parameter (symlink, link and
    rename) the remote chroot must be prepended to such path too.
    
    Ugly hack.

--- src/lib/fuse_api/symlink.c
+++ src/lib/fuse_api/symlink.c
@@ -3,7 +3,20 @@
 int httpfs_symlink( const char *path ,
                     const char *newpath )
 {
-    struct raw_data raw_data = { ( char * )newpath , strlen( newpath ) };
+    struct httpfs *httpfs = fuse_get_context()->private_data;
+    char aux[ 4096 ] = { 0 };
+    struct raw_data raw_data = { aux , 0 };
+
+    /* prepend remote chroot */
+    if ( httpfs->remote_chroot )
+    {
+        raw_data.size = strlen( httpfs->remote_chroot );
+        strcat( raw_data.payload , httpfs->remote_chroot );
+    }
+
+    /* append the other path  */
+    strcat( raw_data.payload , newpath );
+    raw_data.size += strlen( newpath );
 
     HTTPFS_DO_REQUEST_WITH_DATA( HTTPFS_OPCODE_symlink )
     {
